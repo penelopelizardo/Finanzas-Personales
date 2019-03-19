9666d53f242e78b44414a934626addf5696d3f7c
PGR\penelope.lizardo <penelope.lizardo>
2019-03-19 09:10:00 -0400

    Add project files.

---
+++ FinanzasPersonalesWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FinanzasPersonalesWeb.MvcApplication" Language="C#" %>
---
+++ FinanzasPersonalesWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinanzasPersonalesWeb.Startup))]
+namespace FinanzasPersonalesWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
